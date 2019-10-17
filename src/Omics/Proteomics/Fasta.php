0870acce4a504d2e702d91330b72e13b9be121f6
jeffsocal <jeff@socalbioinformatics.com>
2019-10-17 14:33:35 -0700

    updates

--- src/Omics/Proteomics/Fasta.php
+++ src/Omics/Proteomics/Fasta.php
@@ -24,11 +24,11 @@ class Fasta
         $ini = parse_ini_file(get_include_path() . 'ini/molecular.ini');
         $this->fasta_file_path = $ini['fasta_path'];
         $this->fasta_file_name = preg_replace("/.*\//", "", $this->fasta_file_path);
-        
+
         $source_regex = 'uniprot';
         if (preg_match("/uniref/", $source))
             $source_regex = 'uniref';
-        
+
         $this->regex_array = array();
         foreach ($ini as $key => $value) {
             if (strstr($key, $source_regex . '_regex_'))
@@ -49,13 +49,17 @@ class Fasta
     function parseFastaEntry($entry)
     {
         $array = array();
-        
+
         foreach ($this->regex_array as $regex_var => $regex_val) {
-            
+
             $array[$regex_var] = '';
-            
+
+            if (is_array($entry)) {
+                print_r($entry);
+                exit();
+            }
             preg_match('/' . $regex_val . '/', $entry, $regex);
-            
+
             if (key_exists(0, $regex))
                 $array[$regex_var] = trim(preg_replace('/\w+\=|\n/', '', $regex[0]));
         }
@@ -71,26 +75,26 @@ class Fasta
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
                 // process the line read.
-                
+
                 if (preg_match('/^>.+' . $uniprot_name . '/', $line))
                     $continue = TRUE;
-                
+
                 if ($continue == TRUE) {
-                    
+
                     if (preg_match('/^>/', $line)) {
-                        
+
                         if ($entry != FALSE)
                             break;
-                        
+
                         $entry = trim($line, '>');
                     } else {
                         $entry .= $line;
                     }
                 }
             }
-            
+
             return $this->parseFastaEntry($entry);
-            
+
             fclose($handle);
         } else {
             // error opening the file.
@@ -101,26 +105,26 @@ class Fasta
     {
         $entry = FALSE;
         $continue = FALSE;
-        
+
         $n = 0;
-        
+
         $handle = fopen($this->fasta_file_path, "r");
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
                 // process the line read.
-                
+
                 if (preg_match('/^>/', $line)) {
                     $n ++;
                     if ($entry != FALSE & $n == $number)
                         break;
-                    
+
                     $entry = trim($line, '>');
                 } else {
                     $entry .= $line;
                 }
             }
             return $this->parseFastaEntry($entry);
-            
+
             fclose($handle);
         } else {
             // error opening the file.
@@ -131,20 +135,20 @@ class Fasta
     {
         $entry = FALSE;
         $continue = FALSE;
-        
+
         $protein_hits = array();
-        
+
         $handle = fopen($this->fasta_file_path, "r");
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
                 // process the line read.
-                
+
                 if (preg_match('/^>/', $line)) {
-                    
+
                     $protein = $this->parseFastaEntry($entry);
                     if (preg_match('/' . $peptide . '/', $protein['seq']))
                         $protein_hits[] = $protein;
-                    
+
                     $entry = trim($line, '>');
                 } else {
                     $entry .= $line;
@@ -153,7 +157,7 @@ class Fasta
             $protein = $this->parseFastaEntry($entry);
             if (preg_match('/' . $peptide . '/', $protein['seq']))
                 $protein_hits[] = $protein;
-            
+
             fclose($handle);
         } else {
             // error opening the file.
@@ -165,14 +169,14 @@ class Fasta
     {
         $entry = FALSE;
         $parsed = [];
-        
+
         if (! is_false($progress)) {
             $tmr = new ProgressTimer();
-            
+
             $tmr->proTimerSize(556825);
             $tmr->proTimerStart("Parse FASTA");
         }
-        
+
         $handle = fopen($this->fasta_file_path, "r");
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
@@ -183,10 +187,10 @@ class Fasta
                     if (! is_false($entry)) {
                         if (! is_false($progress))
                             $tmr->proTimerPrint();
-                        
+
                         $parsed[] = $this->parseFastaEntry($entry);
                     }
-                    
+
                     $entry = trim($line, '>');
                 } else {
                     $entry .= $line;
@@ -196,12 +200,12 @@ class Fasta
              * get the last one
              */
             $parsed[] = $this->parseFastaEntry($entry);
-            
+
             fclose($handle);
         } else {
             // error opening the file.
         }
-        
+
         return $parsed;
     }
 }
